5359d517e63d93a37ebc763a131db1f1aa3e6f96
satoshi0619 <150905126+satoshi0619@users.noreply.github.com>
2023-11-15 21:00:35 +0900

    app.py

--- app.py
+++ app.py
@@ -4,102 +4,54 @@ import openai
 # OpenAI APIキーの設定
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
-# ゲームの初期設定
-if "game_state" not in st.session_state:
-    st.session_state["game_state"] = {
-        "location": "始まりの村",
-        "remaining_actions": 10,
-        "companions": 0,
-        "game_over": False,
-        "story": "あなたは冒険者かかかちゃん。剣と魔法の世界で冒険をして、美少女を仲間にする旅に出ます。"
+# チャットボットの感情パラメーターの初期設定
+if "emotions" not in st.session_state:
+    st.session_state["emotions"] = {
+        "喜び": 0,
+        "怒り": 0,
+        "悲しみ": 0,
+        "楽しさ": 0,
+        "自信": 0,
+        "困惑": 0,
+        "恐怖": 0
     }
 
-# ゲームの現在の状態を取得
-game_state = st.session_state["game_state"]
-
-# 選択肢を提示する関数
-def present_choices(story):
-    # ストーリーの内容に基づいて選択肢を決定
-    if "森" in story:
-        choices = [
-            "深い森の奥へ進む",
-            "森の中の洞窟を探検する",
-            "森を離れて村へ戻る"
-        ]
-    elif "村" in story:
-        choices = [
-            "村の市場で物資を調達する",
-            "村の宿屋で休憩する",
-            "村を離れて次の冒険へ"
-        ]
-    else:
-        choices = [
-            "隣の村へ旅を続ける",
-            "近くの森を探検する",
-            "近くの川で休憩する"
-        ]
-    return choices
-
-# ここで present_choices を呼び出す
-choices = present_choices(game_state["story"])
-if "choice" not in st.session_state:
-    st.session_state["choice"] = choices[0]
-
 # ゲームの進行に関する関数
-def play_game():
-    choice = st.session_state["choice"]
-    game_state = st.session_state["game_state"]
+def play_game(user_input):
+    emotions = st.session_state["emotions"]
+    
+    # 感情パラメーターの更新ロジック（ダミーの例）
+    # 実際にはユーザー入力やゲームの状況に応じて感情を更新するロジックを実装する
+    # ここではシンプルな例として、固定値で更新します
+    emotions["喜び"] = min(emotions["喜び"] + 1, 5)
+    emotions["楽しさ"] = min(emotions["楽しさ"] + 1, 5)
+    
+    # 感情パラメーターの表示
+    st.write("&#8203;``【oaicite:1】``&#8203;")
+    for emotion, value in emotions.items():
+        st.write(f"{emotion}:{value}")
 
-    # ゲームオーバーのチェック
-    if game_state["game_over"]:
-        st.write("ゲームは終了しました。")
-        return
-
-    # 行動回数の更新
-    game_state["remaining_actions"] -= 1
-
-    # ゲームオーバーの条件をチェック
-    if game_state["remaining_actions"] <= 0:
-        game_state["game_over"] = True
-        st.write("残り行動回数がなくなりました。冒険は終わりです。")
-        return
-
-    # OpenAIを使用してゲームストーリーを進行
+    # OpenAIを使用して会話を進行
     response = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
             {"role": "system", "content": "ゲームマスター"},
-            {"role": "user", "content": choice}
+            {"role": "user", "content": user_input}
         ],
-        max_tokens=500  # max_tokensを小さく設定
+        max_tokens=150
     )
-
-    # 新しいストーリーの追加
-    new_story = response.choices[0].message["content"]
-
-    # 応答を要約するコード（ここで要約アルゴリズムを実装する）
-    # 例として、応答の最初の文だけを取得する単純な要約を行います。
-    summary = new_story.split('.')[0] + '.' if '.' in new_story else new_story
-
-    # 要約されたストーリーを追加
-    if len(summary) > 500:
-        summary = summary[:500] + "..."  # 150文字に切り詰め
-
-    game_state["story"] += "\n" + summary
+    
+    # 応答の表示
+    bot_response = response.choices[0].message["content"]
+    st.write("&#8203;``【oaicite:0】``&#8203;")
+    st.write(bot_response)
 
 # ユーザーインターフェイスの構築
-st.title("冒険の旅へようこそ！")
-st.write("美少女を仲間にし、冒険を進めましょう！")
+st.title("感情を持つチャットボットとの会話")
 
-# 選択肢の変更時に play_game を呼び出す
-st.radio("どうする？", choices, key="choice", on_change=play_game)
+# ユーザーからの入力を受け取る
+user_input = st.text_input("何を話し合いましょうか？")
 
-# ゲームの現在の状態を表示
-st.write("場所: ", game_state["location"])
-st.write("残り行動回数: ", game_state["remaining_actions"])
-st.write("仲間の数: ", game_state["companions"])
-st.write("ストーリー: ", game_state["story"])
-
-# ゲームオーバーの処理
-if game_state["game_over"]:
-    st.write("ゲームオーバーです。")
+# 行動ボタン
+if st.button("話す"):
+    play_game(user_input)
