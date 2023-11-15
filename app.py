c21c9e69d1bce32402e6a71bf429f2eda854e321
satoshi0619 <150905126+satoshi0619@users.noreply.github.com>
2023-11-15 21:18:30 +0900

    app.py

--- app.py
+++ app.py
@@ -10,25 +10,25 @@ name = st.text_input("名前を教えてください～！")
 
 # 褒め言葉を生成する関数
 def generate_compliment(name):
-    try:
-        if name:  # 名前が入力されているか確認
-            # ... (APIリクエストのコード) ...
-    except openai.error.OpenAIError as e:
-        st.error(f"OpenAI APIでエラーが発生しました: {e}")
-        return None
-
-        # OpenAIのAPIを使用して褒め言葉を生成
-        response = openai.Completion.create(
-            model="gpt-3.5-turbo",  # GPT-4モデルを指定
-            prompt=f"私は{ name }を褒めるチャットボットです。{ name }に対してポジティブで心温まる褒め言葉をいくつか教えてください。",
-            temperature=0.7,
-            max_tokens=60,
-            top_p=1.0,
-            frequency_penalty=0.0,
-            presence_penalty=0.0,
-            stop=["\n"]
-        )
-        return response.choices[0].text.strip()  # 生成された褒め言葉を返す
+    if name:  # 名前が入力されているか確認
+        try:
+            # OpenAIのAPIを使用して褒め言葉を生成
+            response = openai.Completion.create(
+                model="gpt-3.5-turbo",  # モデルを指定
+                prompt=f"私は{ name }を褒めるチャットボットです。{ name }に対してポジティブで心温まる褒め言葉をいくつか教えてください。",
+                temperature=0.7,
+                max_tokens=60,
+                top_p=1.0,
+                frequency_penalty=0.0,
+                presence_penalty=0.0,
+                stop=["\n"]
+            )
+            return response.choices[0].text.strip()  # 生成された褒め言葉を返す
+        except openai.error.OpenAIError as e:
+            st.error(f"OpenAI APIでエラーが発生しました: {e}")
+            return None
+    else:
+        return None  # 名前が入力されていなければNoneを返す
 
 # 行動ボタン
 if st.button("褒め言葉を生成"):
@@ -36,4 +36,4 @@ if st.button("褒め言葉を生成"):
     if compliment:  # 褒め言葉が生成されたか確認
         st.success(compliment)
     else:
-        st.error("名前を入力してください。")
+        st.error("名前を入力して、もう一度試してください。")
