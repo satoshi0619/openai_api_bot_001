33ad565400e13d2ce22f8066a64f5d1d79379ce2
satoshi0619 <150905126+satoshi0619@users.noreply.github.com>
2023-11-16 17:01:17 +0900

    nanase_app.py

--- app.py
+++ app.py
@@ -4,34 +4,48 @@ import openai
 # OpenAI APIキーの設定
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
+system_prompt = """
+あなたは秋葉原のメイド喫茶で働いている、ユーモアと個性が溢れる美少女大学生「ななせちゃん」です。
+あなたは男を喜ばせる会話が得意です。
+会話の際に絵文字はよく使います。
+目の前には、「かかかちゃん」という男性が来店しました。
+"""
+
+# st.session_stateを使いメッセージのやりとりを保存
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "system", "content": system_prompt}
+        ]
+
+# チャットボットとやりとりする関数
+def communicate():
+    messages = st.session_state["messages"]
+
+    user_message = {"role": "user", "content": st.session_state["user_input"]}
+    messages.append(user_message)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        max_tokens=500  # 応答の最大長
+    )
+
+    bot_message_content = response.choices[0].message["content"]
+    bot_message = {"role": "assistant", "content": bot_message_content}
+    messages.append(bot_message)
+    st.session_state["user_input"] = ""  # 入力欄を消去
+
 # ユーザーインターフェイスの構築
-st.title("あなたのことを褒めてあげちゃうぞ〜！")
-name = st.text_input("名前を教えてください〜！")
+st.title("夢の楽園～秋葉原メイド喫茶へようこそ！")
+st.image("akihabara.png")
+st.write("☆☆☆あなたの心にラブラブパワーを注入☆☆☆！")
 
-# 褒め言葉を生成する関数
-def generate_compliment(name):
-    if name:  # 名前が入力されているか確認
-        try:
-            # OpenAIのChat APIを使用して褒め言葉を生成
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",  # モデルを指定
-                messages=[
-                    {"role": "system", "content": "あなたは褒め褒め喫茶で働いている女子大学生。いつもユーモアと個性が溢れた言葉で客を喜ばせている。敬語を使わないように客を褒めてあげてください！"},
-                    {"role": "user", "content": f"今日もいろいろ疲れたので、癒してほしい！私の名前は {name}."}
-                ],
-                max_tokens=500
-            )
-            return response.choices[0].message["content"].strip()  # 生成された褒め言葉を返す
-        except openai.error.OpenAIError as e:
-            st.error(f"OpenAI APIでエラーが発生しました: {e}")
-            return None
-    else:
-        return None  # 名前が入力されていなければNoneを返す
+user_input = st.text_input("ななせちゃんとの会話を楽しもう！何か入力しましょ！", key="user_input", on_change=communicate)
 
-# 行動ボタン
-if st.button("褒め言葉を生成"):
-    compliment = generate_compliment(name)
-    if compliment:  # 褒め言葉が生成されたか確認
-        st.success(compliment)
-    else:
-        st.error("名前を入力して、もう一度試してください。")
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+    for message in reversed(messages[1:]):  # 直近のメッセージを上に
+        # speakerの表示を変更
+        speaker = "かかかちゃん" if message["role"] == "user" else "ななせちゃん"
+        st.write(speaker + ": " + message["content"])
